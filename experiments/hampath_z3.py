3b21a4ac74a3a7ed3320d311ae35c86d06ffd70b
mossurtt <mossurtt@gmail.com>
2024-01-27 22:32:33 +0100

    27.01 - kodowanie HAMPATH

--- experiments/hampath_z3.py
+++ experiments/hampath_z3.py
@@ -1,69 +1,95 @@
-from z3 import *
+import z3 
 
-def hamiltonian_path(graph):
+def main():
+
+    digraph = {
+        0: [1, 4, 5],
+        1: [0, 7, 2],
+        2: [1, 9, 3],
+        3: [2, 11, 4],
+        4: [3, 13, 0],
+        5: [0, 14, 6],
+        6: [5, 16, 7],
+        7: [6, 8, 1],
+        8: [7, 17, 9],
+        9: [8, 10, 2],
+        10: [9, 18, 11],
+        11: [10, 3, 12],
+        12: [11, 19, 13],
+        13: [12, 14, 4],
+        14: [13, 15, 5],
+        15: [14, 16, 19],
+        16: [6, 17, 15],
+        17: [16, 8, 18],
+        18: [10, 19, 17],
+        19: [18, 12, 15]
+    }
+    
+    hamiltonian_path(digraph)
+    hamiltonian_path(graf)
+
+def hamiltonian_path(graph: dict[int, list[int]]):
 
     n = len(graph)
+    vertice = z3.IntVector("v", n)
+
+    solver = z3.Solver()
+
+    solver.add(proper_numbers(vertice))
+
+    solver.add(distinct_vs(vertice))
+
+    edges = []
+    for i in range(n - 1):
+        edges.append(edge(graph, vertice[i], vertice[i + 1]))
+    solver.add(z3.And(edges))
+
+    smt2_representation = solver.to_smt2()
+    file_name = f'1hampath_state.smt2'
+    with open(file_name, 'w') as file:
+        file.write(smt2_representation)
+    file.close()
+
+    result = solver.check()
+    if result == z3.sat:
+        model = solver.model()
+        vertex_values = [(idx, model[v].as_long()) for idx, v in enumerate(vertice)]
+        sorted_vertices = sorted(vertex_values)
+        print("Hamiltonian Path:")
+        for idx, value in sorted_vertices[:-1]:
+            print(f"v__{value}", end=' -> ')
+        print(f"v__{sorted_vertices[-1][1]}")
+    else:
+        print(result)
     
-    v = [Int(f"v{i}") for i in range(n)]
+def proper_numbers(vertice):
+    n = len(vertice)
+    atoms = []
+    for i in range(n - 1):
+        atoms.append(z3.And(vertice[i] >= 0, vertice[i] < n))
+    bf = z3.And(atoms)
+    z3.simplify(bf)
+    return bf
 
-    s = Solver()
-    s.add(v[0] == 0)
-    
-    for i in range(n):
-        s.add(Or([v[j] == (v[i] + 1) % n for j in graph[i]]))
-    return s
+def distinct_vs(vertice):
+    n = len(vertice)
+    atoms = []
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            atoms.append(vertice[i] != vertice[j])
+    bf = z3.And(atoms)
+    z3.simplify(bf)
+    return bf
 
-def examples():
-    # Dodecahedron
-    grdodec = { 0: [1, 4, 5],
-                1: [0, 7, 2],
-                2: [1, 9, 3],
-                3: [2, 11, 4],
-                4: [3, 13, 0],
-                5: [0, 14, 6],
-                6: [5, 16, 7],
-                7: [6, 8, 1],
-                8: [7, 17, 9],
-                9: [8, 10, 2],
-                10: [9, 18, 11],
-                11: [10, 3, 12],
-                12: [11, 19, 13],
-                13: [12, 14, 4],
-                14: [13, 15, 5],
-                15: [14, 16, 19],
-                16: [6, 17, 15],
-                17: [16, 8, 18],
-                18: [10, 19, 17],
-                19: [18, 12, 15] }
+def edge(graph: dict[int, list[int]], s, t):
+    atoms = []
+    for source in graph:
+        for target in graph[source]:
+            atoms.append(z3.And([s == source, t == target]))
+    bf = z3.Or(atoms)
+    z3.simplify(bf)
+    return bf
 
-    import pprint
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(grdodec)
-
-    sdodec = hamiltonian_path(grdodec)
-    print(sdodec.check())
-    path = sdodec.model()
-    if (sdodec.check() == sat): print(" -> ".join(f"{i}" for i in path))
-    else: return None
-
-    # The Herschel graph is the smallest possible polyhedral graph that does not have a Hamiltonian cycle.
-    grherschel = { 0: [1, 9, 10, 7],
-                   1: [0, 8, 2],
-                   2: [1, 9, 3],
-                   3: [2, 8, 4],
-                   4: [3, 9, 10, 5],
-                   5: [4, 8, 6],
-                   6: [5, 10, 7],
-                   7: [6, 8, 0],
-                   8: [1, 3, 5, 7],
-                   9: [2, 0, 4],
-                   10: [6, 4, 0] }
-
-    pp.pprint(grherschel)
-    sherschel = hamiltonian_path(grherschel)
-    print(sherschel.check())
-    if (sherschel.check() == sat): print(sherschel.model())
-    else: return None
 
 if __name__ == "__main__":
-    examples()
+    main()
