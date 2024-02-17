dfac5af8185e0772247d9631f03c3fa6386741d5
mossurtt <mossurtt@gmail.com>
2024-02-17 19:28:17 +0100

    17.02 - opis kodowania HAMPATH

--- experiments/hampath_z3.py
+++ experiments/hampath_z3.py
@@ -1,43 +1,10 @@
 import z3 
 
 def main():
-
-    filename = "unsatgraph.txt"
+    filename = "digraph.txt"
     digraph = read_graph_from_file(filename)
     
-
-    # digraph = {
-    #     0: [1, 4, 5],
-    #     1: [0, 7, 2],
-    #     2: [1, 9, 3],
-    #     3: [2, 11, 4],
-    #     4: [3, 13, 0],
-    #     5: [0, 14, 6],
-    #     6: [5, 16, 7],
-    #     7: [6, 8, 1],
-    #     8: [7, 17, 9],
-    #     9: [8, 10, 2],
-    #     10: [9, 18, 11],
-    #     11: [10, 3, 12],
-    #     12: [11, 19, 13],
-    #     13: [12, 14, 4],
-    #     14: [13, 15, 5],
-    #     15: [14, 16, 19],
-    #     16: [6, 17, 15],
-    #     17: [16, 8, 18],
-    #     18: [10, 19, 17],
-    #     19: [18, 12, 15]
-    # }
-
-#     unsatgraph = {
-#     0: [1, 2],
-#     1: [0, 2, 3],
-#     2: [0, 1],
-#     3: [1],
-#     4: [1]
-# }
-    
-    hamiltonian_path(digraph)
+    check_hampath(digraph)
 
 def read_graph_from_file(filename):
     with open(filename, 'r') as file:
@@ -50,20 +17,20 @@ def read_graph_from_file(filename):
 
     return graph
 
-def hamiltonian_path(graph: dict[int, list[int]]):
+def check_hampath(graph: dict[int, list[int]]):
 
     n = len(graph)
-    vertice = z3.IntVector("v", n)
+    vertices = z3.IntVector("v", n)
 
     solver = z3.Solver()
 
-    solver.add(proper_numbers(vertice))
+    solver.add(proper_numbers(vertices))
 
-    solver.add(distinct_vs(vertice))
+    solver.add(distinct_vs(vertices))
 
     edges = []
     for i in range(n - 1):
-        edges.append(edge(graph, vertice[i], vertice[i + 1]))
+        edges.append(edge(graph, vertices[i], vertices[i + 1]))
     solver.add(z3.And(edges))
 
     smt2_representation = solver.to_smt2()
@@ -76,32 +43,30 @@ def hamiltonian_path(graph: dict[int, list[int]]):
     result = solver.check()
     if result == z3.sat:
         model = solver.model()
-        vertex_values = [(idx, model[v].as_long()) for idx, v in enumerate(vertice)]
-        sorted_vertices = sorted(vertex_values)
+        vertex_values = [(idx, model[v].as_long()) for idx, v in enumerate(vertices)]
+        sorted_verticess = sorted(vertex_values)
         print("Hamiltonian Path:")
-        for idx, value in sorted_vertices[:-1]:
+        for idx, value in sorted_verticess[:-1]:
             print(f"v__{value}", end=' -> ')
-        print(f"v__{sorted_vertices[-1][1]}")
+        print(f"v__{sorted_verticess[-1][1]}")
     else:
         print(result)
     
-def proper_numbers(vertice):
-    n = len(vertice)
+def proper_numbers(vertices):
+    n = len(vertices)
     atoms = []
     for i in range(n - 1):
-        atoms.append(z3.And(vertice[i] >= 0, vertice[i] < n))
+        atoms.append(z3.And(vertices[i] >= 0, vertices[i] < n))
     bf = z3.And(atoms)
-    z3.simplify(bf)
     return bf
 
-def distinct_vs(vertice):
-    n = len(vertice)
+def distinct_vs(vertices):
+    n = len(vertices)
     atoms = []
     for i in range(n - 1):
         for j in range(i + 1, n):
-            atoms.append(vertice[i] != vertice[j])
+            atoms.append(vertices[i] != vertices[j])
     bf = z3.And(atoms)
-    z3.simplify(bf)
     return bf
 
 def edge(graph: dict[int, list[int]], s, t):
@@ -110,7 +75,6 @@ def edge(graph: dict[int, list[int]], s, t):
         for target in graph[source]:
             atoms.append(z3.And([s == source, t == target]))
     bf = z3.Or(atoms)
-    z3.simplify(bf)
     return bf
 
 
