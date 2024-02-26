14dae50ab9351ecdf0dadd1e6f61dd3395a9f5b2
mossurtt <mossurtt@gmail.com>
2024-02-26 21:16:46 +0100

    26.02 - zamiana BoolVector na IntVector

--- experiments/maxclique.py
+++ experiments/maxclique.py
@@ -1,7 +1,12 @@
+import sys
 import z3
 
 def main():
-    filename = "inputs/clique.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 maxclique.py <filename>")
+        return
+
+    filename = sys.argv[1]
     graph = read_graph_from_file(filename)
     maxclique = check_maxclique(graph)
     if maxclique:
@@ -23,10 +28,11 @@ def read_graph_from_file(filename):
 def check_maxclique(graph):
     n = len(graph)
     
-    vertices = z3.BoolVector('v', n)
+    vertices =  z3.IntVector('v', n)
     
     solver = z3.Solver()
-    
+    solver.add(proper_numbers(vertices))
+    solver.add(distinct_vs(vertices))
     max_size = 0
     left = 0
     right = n
@@ -34,12 +40,12 @@ def check_maxclique(graph):
     while left <= right:
         mid = (left + right) // 2
         solver.reset()
-        solver.add(z3.Sum([z3.If(vertices[i], 1, 0) for i in range(n)]) <= mid)
+        solver.add(z3.Sum([z3.If(v > 1, 1, 0) for v in vertices]) <= mid)
         
         for i in range(n):
             for j in range(i + 1, n):
                 if j not in graph[i]: 
-                    solver.add(z3.Implies(z3.And(vertices[i], vertices[j]), z3.Or(z3.Not(vertices[i]), z3.Not(vertices[j]))))
+                    solver.add(z3.Implies(z3.And(vertices[i] > 0, vertices[j] > 0), z3.Or(vertices[i] <= 0, vertices[j] <= 0)))
         
         if solver.check() == z3.sat:
             max_size = mid
@@ -47,14 +53,32 @@ def check_maxclique(graph):
         else:
             right = mid - 1
     
+    
     smt2_representation = solver.to_smt2()
     file_name = f'maxclique_state.smt2'
     with open(file_name, 'w') as file:
         file.write("(set-logic ALL)\n")
         file.write(smt2_representation)  
     file.close()
-    
-    return [i for i in range(n) if solver.model()[vertices[i]]]
+    print(solver.model())
+    return [i for i in range(n) if solver.model()[vertices[i]].as_long() > 0]
+
+def proper_numbers(vertices):
+    n = len(vertices)
+    atoms = []
+    for i in range(n - 1):
+        atoms.append(z3.And(vertices[i] >= 0, vertices[i] < n))
+    bf = z3.And(atoms)
+    return bf
+
+def distinct_vs(vertices):
+    n = len(vertices)
+    atoms = []
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            atoms.append(vertices[i] != vertices[j])
+    bf = z3.And(atoms)
+    return bf
 
 if __name__ == "__main__":
     main()
