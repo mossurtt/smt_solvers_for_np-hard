16947bcc0224af95efac578469100188b19bf0a7
mossurtt <mossurtt@gmail.com>
2024-03-03 13:54:33 +0100

    03.03 - poprawiono funkcje generowania

--- experiments/Grafy_nieskierowane/generate_graph.py
+++ experiments/Grafy_nieskierowane/generate_graph.py
@@ -1,15 +1,15 @@
 import igraph as ig
+import random
 
 def main():
     for i in range(5, 100, 5):
-        for j in range(3, 30, 2):
-            g = ig.Graph.Barabasi(i,j)
+            g = ig.Graph.Barabasi(i, random.randint(1, 10))
             g.write_edgelist(f'barabasi_{i}.txt')
 
     for i in range(100, 1050, 50):
-        for j in range(3, 30, 2):
-            g = ig.Graph.Barabasi(i, j)
-            g.write_edgelist(f'barabasi_{i}.txt')
+        g = ig.Graph.Barabasi(i, random.randint(10, 50))
+        g.write_edgelist(f'barabasi_{i}.txt')
+        
 
 if __name__ == '__main__':
     main()
