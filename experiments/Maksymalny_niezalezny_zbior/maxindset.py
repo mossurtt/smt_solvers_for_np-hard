f0309b381c6ea1bac09720c08593579f63f6f401
mossurtt <mossurtt@gmail.com>
2024-03-07 21:36:50 +0100

    07.03 - poprawiony maxindset.py

--- experiments/Maksymalny_niezalezny_zbior/maxindset.py
+++ experiments/Maksymalny_niezalezny_zbior/maxindset.py
@@ -10,38 +10,36 @@ def main():
 
     filename = sys.argv[1]
     graph = read_graph_from_file(filename)
-
-    maxindset = check_maxindset(graph)
-    if maxindset:
-        print('Maximal independent set:', maxindset)
-        print('Size:', len(maxindset))
-    else:
-        print('unsat')
-
-def check_maxindset(graph: dict[int, list[int]]):
-
     n = len(graph)
-    k = 1
 
     for k in range(2, n + 1):
-        vertices = z3.IntVector("v", n)
-        solver = z3.Solver()
-
-        solver.add(proper_numbers(vertices))
-        solver.add(distinct_vs(vertices))
-            
-        no_edges = []
-        for i in range(k):
-            for j in range(i + 1, k):
-                    no_edges.append(z3.Not(edge(graph, vertices[i], vertices[j])))
-        solver.add(z3.And(no_edges))
-
-        result = solver.check() 
-        if result == z3.sat:
-            model = solver.model()
-            maxindset = [model[vertices[i]].as_long() for i in range(k)]
-        else:
+        result, model = check_maxindset(graph, k)
+        if result != z3.sat:
             break
+
+def check_maxindset(graph: dict[int, list[int]], k):
+
+    n = len(graph)
+
+    vertices = z3.IntVector("v", n)
+    solver = z3.Solver()
+
+    solver.add(proper_numbers(vertices))
+    solver.add(distinct_vs(vertices))
+        
+    no_edges = []
+    for i in range(k):
+        for j in range(i + 1, k):
+                no_edges.append(z3.Not(edge(graph, vertices[i], vertices[j])))
+    solver.add(z3.And(no_edges))
+
+    result = solver.check() 
+    if result == z3.sat:
+        model = solver.model()
+        maxindset = [model[vertices[i]].as_long() for i in range(k)]
+        print(maxindset)
+    else:
+        model = None
 
     smt2_representation = solver.to_smt2()
     file_name = f'maxindset_state.smt2'
@@ -50,7 +48,7 @@ def check_maxindset(graph: dict[int, list[int]]):
         file.write(smt2_representation)  
     file.close()
 
-    return maxindset
+    return result, model
 
 
 if __name__ == "__main__":
