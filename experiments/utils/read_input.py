6736da6ec30e87a86e3da5a58f536d9f4f64da84
mossurtt <mossurtt@gmail.com>
2024-03-08 22:38:02 +0100

    08.03 - poprawki

--- experiments/utils/read_input.py
+++ experiments/utils/read_input.py
@@ -44,3 +44,9 @@ def read_wgraph_from_file(filename):
         wgraph[target].append((source, weight))
 
     return wgraph
+
+def read_set_from_file(filename):
+    with open(filename, 'r') as file:
+        data = file.read().strip()
+        input_set = list(map(int, data.split()))
+    return input_set
