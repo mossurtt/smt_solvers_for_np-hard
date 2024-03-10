34e12ed89ad5a5c6ac6d35ce31b4e275443d5600
Andrzej Zbrzezny <a.zbrzezny@gmail.com>
2024-03-10 09:19:17 +0100

    Chwilowo dodany podkatalog codes

---
+++ thesis/codes/read_graph_from_file.py
@@ -0,0 +1,16 @@
+def read_graph_from_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    graph = {}
+    for line in lines:
+        source, target = [int(x) for x in line.strip().split()]
+        if source not in graph:
+            graph[source] = []
+        if target not in graph:
+            graph[target] = []
+        graph[source].append(target)
+        graph[target].append(source)
+
+    return graph
+
