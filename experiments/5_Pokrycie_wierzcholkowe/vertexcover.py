68ea00df6a6e81e13d204e42ae610748ceadd6fc
mossurtt <mossurtt@gmail.com>
2024-03-11 22:34:35 +0100

    program vertexcover

--- experiments/5_Pokrycie_wierzcholkowe/vertexcover.py
+++ experiments/5_Pokrycie_wierzcholkowe/vertexcover.py
@@ -1,6 +1,6 @@
 import sys
 import z3 
-from utils import proper_numbers, distinct_vs
+from utils.constraints import proper_numbers
 from utils.read_input import read_graph_from_file
 
 def main():
@@ -12,28 +12,27 @@ def main():
     graph = read_graph_from_file(filename)
     n = len(graph)
 
-    for k in range(1, n + 1):
+    for k in range(n, 0, -1):
         result, model = check_vertexcover(graph, k)
-        if result != z3.unsat:
+        if result != z3.sat:
             break
 
 def check_vertexcover(graph: dict[int, list[int]], k):
-
     n = len(graph)
     vertices = z3.IntVector("v", n)
     solver = z3.Solver()
-
+    
     solver.add(proper_numbers(vertices))
-    solver.add(distinct_vs(vertices))
+    solver.add(distinct_vs(vertices, k))
 
     vertex_in_cover = []
     for i in range(k):
         for j in range(i + 1, k):
-            vertex_in_cover.append(edge_covered(graph, vertices[i], vertices[j]))
+            vertex_in_cover.append(edge_covered(graph, vertices[i]))
     solver.add(z3.Or(vertex_in_cover))
 
     smt2_representation = solver.to_smt2()
-    file_name = f'vertexcover_state.smt2'
+    file_name = f'vertexcover_{n}.smt2'
     with open(file_name, 'w') as file:
         file.write("(set-logic ALL)\n")
         file.write(smt2_representation)  
@@ -51,11 +50,19 @@ def check_vertexcover(graph: dict[int, list[int]], k):
 
     return result, model
 
-def edge_covered(graph: dict[int, list[int]], s, t):
+def distinct_vs(vertices, k):
+    atoms = []
+    for i in range(k):
+        for j in range(i + 1, k):
+            atoms.append(vertices[i] != vertices[j])
+    bf = z3.And(atoms)
+    return bf
+
+def edge_covered(graph: dict[int, list[int]], v):
     atoms = []
     for source in graph:
         for target in graph[source]:
-            atoms.append(z3.Or([s == source, t == target]))  
+            atoms.append(z3.Or([v == source, v == target]))  
     bf = z3.And(atoms)
     z3.simplify(bf)
     return bf
