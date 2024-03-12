160017f91f7809783da8aa4c3d36d5696323ddba
mossurtt <mossurtt@gmail.com>
2024-03-12 17:00:55 +0100

    poprawiony vertexcover.py

--- experiments/5_Pokrycie_wierzcholkowe/vertexcover.py
+++ experiments/5_Pokrycie_wierzcholkowe/vertexcover.py
@@ -1,6 +1,6 @@
 import sys
 import z3 
-# from utils.constraints import proper_numbers
+from utils.constraints import proper_numbers, distinct_vs
 from utils.read_input import read_graph_from_file
 
 def main():
@@ -19,18 +19,14 @@ def main():
 
 def check_vertexcover(graph: dict[int, list[int]], k):
     n = len(graph)
-    vertices = [z3.Int(f'v_{i}') for i in range(k)]
+    vertices = z3.IntVector('v', k)
     solver = z3.Solver()
-    
-    for i in range(k):
-        solver.add(z3.And(vertices[i] >= 0, vertices[i] < n))
 
-    for i in range(k):
-        for j in range(i + 1, k):
-            solver.add(vertices[i] != vertices[j])
+    solver.add(proper_numbers(vertices))
+    solver.add(distinct_vs(vertices))
 
-    for source, targets in graph.items():
-        for target in targets:
+    for source in graph:
+        for target in graph[source]:
             vertex_in_cover = False
             for j in range(k):
                 vertex_in_cover = z3.Or(vertex_in_cover, z3.Or(vertices[j] == source, vertices[j] == target))
